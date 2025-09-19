db649b4d7dcaf8c7f02ebaf4736d2e1049ac76ec
esdawood <esoodawood186@gmail.com>
2025-09-19 22:50:34 +0300

    Refactor LanguageToggle widget for improved layout and styling
    - Updated the LanguageToggle widget to use a Row layout for better alignment and spacing.
    - Enhanced styling by incorporating AppTextStyles for text consistency.
    - Simplified the container structure and improved the overall readability of the code.
    - Maintained localization support for language options in both Arabic and English.

--- lib/core/widgets/language_toggle.dart
+++ lib/core/widgets/language_toggle.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../services/localization_service.dart';
 import '../helper/localization_helper.dart';
+import '../theme/app_text_styles.dart';
 
 class LanguageToggle extends StatelessWidget {
   const LanguageToggle({super.key});
@@ -15,63 +16,49 @@ class LanguageToggle extends StatelessWidget {
         final localizationService = LocalizationService.instance;
         final isArabic = localizationService.isArabic;
 
-        return Container(
-          padding: const EdgeInsets.symmetric( vertical: 8),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.08),
-                blurRadius: 4,
-                offset: const Offset(0, 1),
+        return Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Image.asset('assets/images/icons/globe.png',
+                width: 44.w, height: 44.h),
+            SizedBox(
+              width: 16.w,
+            ),
+            Text(
+              LocalizationHelper.getLocalizedText('اللغة', 'Language'),
+              textAlign: LocalizationService.instance.textAlignment,
+              textDirection: LocalizationService.instance.textDirection,
+              style: AppTextStyles.font14CharlestonGreenMediumLamaSans,
+            ),
+            Spacer(),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadiusGeometry.circular(8),
+                color: AppColors.lavenderBlush,
               ),
-            ],
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Image.asset('assets/images/icons/globe.png',width: 44.w, height: 44.h),
-              SizedBox(width: 16.w,),
-              Text(
-                LocalizationHelper.getLocalizedText('اللغة', 'Language'),
-                style: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.grey,
-                ),
+              child: Row(
+                children: [
+                  _buildLanguageOption(
+                    context,
+                    'ع',
+                    'AR',
+                    'ar',
+                    isArabic,
+                    localizationService,
+                  ),
+                  const SizedBox(width: 4),
+                  _buildLanguageOption(
+                    context,
+                    'EN',
+                    'EN',
+                    'en',
+                    !isArabic,
+                    localizationService,
+                  ),
+                ],
               ),
-              Spacer(),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadiusGeometry.circular(8),
-                  color: AppColors.lavenderBlush,
-
-                ),
-                child: Row(
-                  children: [
-                    _buildLanguageOption(
-                      context,
-                      'ع',
-                      'AR',
-                      'ar',
-                      isArabic,
-                      localizationService,
-                    ),
-                    const SizedBox(width: 4),
-                    _buildLanguageOption(
-                      context,
-                      'EN',
-                      'EN',
-                      'en',
-                      !isArabic,
-                      localizationService,
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         );
       },
     );
@@ -96,9 +83,9 @@ class LanguageToggle extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
         decoration: BoxDecoration(
-          color: isSelected ? AppColors.philippineBronze : AppColors.lavenderBlush,
+          color:
+              isSelected ? AppColors.philippineBronze : AppColors.lavenderBlush,
           borderRadius: BorderRadius.circular(15),
-
         ),
         child: Text(
           LocalizationHelper.getLocalizedText(arabicText, englishText),
